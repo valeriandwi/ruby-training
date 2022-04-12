cc9acd550c04d9ed969362bd60f6e0652ec85a7b
Valerian Dwi Purnomo <valerian.dwi.p@gmail.com>
2022-04-12 21:52:37 +0700

    Add Sorting & Delete Array

---
+++ Beginner/delete_array.rb
@@ -0,0 +1,6 @@
+colors = ["red","green","blue"]
+
+colors.delete_at(1);
+colors.delete("blue");
+
+puts colors;
---
+++ Beginner/sorting.rb
@@ -0,0 +1,4 @@
+numbers = [1, 4, 6, 7, 3, 2, 5]
+
+puts numbers.sort!
+puts numbers.reverse
